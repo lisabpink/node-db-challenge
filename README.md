f68d86690df39f7193b19e2353bf096fe7acce15
LISA_COOK <lisabpink@gmail.com>
2020-03-20 12:40:20 -0400

    LDC added mvp endpoints:retrieving a list of resources, projects & tasks

--- README.md
+++ README.md
@@ -65,13 +65,13 @@ Complete the following tasks:
   - [ X ] when adding a `task` the client must provide a description, the notes are optional.
   - [ X ] when adding a `task` the client must provide the `id` of an existing project.
   - [ X ] for `projects` and `tasks` if no value is provided for the `completed` property, the API should provide a default value of `false`.
-- [ ] Build an API with endpoints for:
+- [ X ] Build an API with endpoints for:
   - [ ] adding resources.
-  - [ ] retrieving a list of resources.
+  - [ X ] retrieving a list of resources.
   - [ ] adding projects.
-  - [ ] retrieving a list of projects.
+  - [ X ] retrieving a list of projects.
   - [ ] adding tasks.
-  - [ ] retrieving a list of tasks. **The list of tasks should include the project name and project description**.
+  - [ X ] retrieving a list of tasks. **The list of tasks should include the project name and project description**.
 
 ### Entities
 
--- projects/projectModel.js
+++ projects/projectModel.js
@@ -1,9 +1,42 @@
-const db = require('../data/db-config');
+const db = require("../data/db-config");
 
 module.exports = {
-    getProjects
-}
+  getProjects,
+  getProjectById,
+  getResourcesByProjectId,
+  getTaskByProjectId
+};
 
 function getProjects() {
-    return db('project');
-} 
+  return db("project");
+}
+
+function getProjectById(id) {
+  return db("project").where({ id });
+}
+
+function getResourcesByProjectId(id) {
+  return db("project_resources as pr")
+    .join("resource as r", "pr.resource_id", "r.id")
+    .join("project as p", "pr.project_id", "p.id")
+    .select(
+      "p.name as project-name",
+      "r.id",
+      "r.name as resource-name",
+      "r.description"
+    )
+    .where("pr.project_id", id);
+}
+
+function getTaskByProjectId(id) {
+  return db("task as t")
+    .join("project as p", "t.project_id", "p.id")
+    .select(
+      "p.name as project-name",
+      "p.description as project-description",
+      "t.id",
+      "t.description",
+      "t.notes"
+    )
+    .where("t.project_id", id);
+}
